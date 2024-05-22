cf9f8881948c6b789d75d79813af31797ea2611f
karthik <karthik.mahadeva@veolia.com>
2024-05-22 18:28:18 +0530

    rr

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -3,7 +3,6 @@ import './dashboard.css';
 import Video from "../../assests/video.mp4";
 const dashboard = () => {
     return (
-
         <video muted autostart autoPlay src={Video} type="video/mp4" />
     )
 }
